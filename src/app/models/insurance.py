2fba47519e2d3eb986785fe9d89abcf05de51604
ex <specialized8393@gmail.com>
2026-01-19 18:50:36 +0800

    add relationship to Fund model as funds field
    back_populates="insurance"

---
+++ src/app/models/insurance.py
@@ -0,0 +1,33 @@
+import uuid as uuid_pkg
+from datetime import UTC, datetime
+from enum import Enum
+
+from sqlalchemy import Boolean, DateTime, String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from uuid6 import uuid7
+
+from ..core.db.database import Base
+
+class InsuranceStatusEnum(str, Enum):
+    CONNECTED = "CONNECTED"
+    DISCONNECTED = "DISCONNECTED"
+
+
+class Insurance(Base):
+    __tablename__ = "insurance"
+
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True, init=False)
+
+    name: Mapped[str] = mapped_column(String(50), nullable=False, unique=True, index=True)
+    email: Mapped[str] = mapped_column(String(50), nullable=False)
+    policy_id: Mapped[int] = mapped_column(String(100), nullable=False, unique=True, index=True)
+    status: Mapped[InsuranceStatusEnum] = mapped_column(String(50), default=InsuranceStatusEnum.DISCONNECTED)
+
+    uuid: Mapped[uuid_pkg.UUID] = mapped_column(UUID(as_uuid=True), default_factory=uuid7, unique=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default_factory=lambda: datetime.now(UTC))
+    updated_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), default=None)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False, index=True)
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), default=None)
+
+    funds: Mapped[list["Fund"]] = relationship("Fund", back_populates="insurance", init=False, default_factory=list)
